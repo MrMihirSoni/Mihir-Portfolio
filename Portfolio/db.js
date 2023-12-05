f6d8973ffda3debc27ed72d61c86c9f9e528f902
Mihir Soni <ms6711534@gmail.com>
2023-12-05 16:05:31 +0530

    hardware

--- Portfolio/db.js
+++ Portfolio/db.js
@@ -2,21 +2,21 @@
 const projects = [
     {
         title: "Envoy Copy",
-        image: "./public/Envoy.jpg",
+        image: "../public/Envoy.jpg",
         description: "In this website I tried to copy the official website of 'Envoy'. Hope you like my efforts.",
         deploymentLink: "https://envoy-copy.vercel.app/",
         gitRepo: "https://github.com/MrMihirSoni/EnvoyCopy"
     },
     {
         title: "Basic Calculator",
-        image: "./public/Calculator.jpg",
+        image: "../public/Calculator.jpg",
         description: "In this website  I made a basic Calculator by just using HTML, CSS and JavaScript. Hope you like it.",
         deploymentLink: "https://effortless-banoffee-e76d49.netlify.app/",
         gitRepo: "https://github.com/MrMihirSoni/Basic-Calculator"
     },
     {
         title: "OMDB",
-        image: "./public/Omdb.jpg",
+        image: "../public/Omdb.jpg",
         description: "In this website I made a simple movie search engine, where you can see the rating/about/description of the movies yourself.",
         deploymentLink: "https://jovial-sunburst-a20f02.netlify.app/",
         gitRepo: "https://jovial-sunburst-a20f02.netlify.app/"
