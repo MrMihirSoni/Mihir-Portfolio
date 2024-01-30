28d2ceefa258cea37a68d62cbaa8813d562fd963
Mihir Soni <ms6711534@gmail.com>
2024-01-30 15:17:40 +0530

    bug fixxes

--- Portfolio/src/introComponent/Intro.jsx
+++ Portfolio/src/introComponent/Intro.jsx
@@ -15,7 +15,7 @@ const Intro = () => {
                 <p>I'm <span className='myName'>Mihir Soni</span></p>
                 <p>FrontEnd Developer</p>
                 <div className='homeBtnDiv'>
-                <a href='Mihir-Soni-Resume.pdf' download='Mihir-Soni-Resume.pdf'><button className='homeBtn' onClick={handleResume}>Resume</button></a>
+                <a href={MihirSoniResume} download='Mihir-Soni-Resume.pdf'><button className='homeBtn' onClick={handleResume}>Resume</button></a>
                 </div>
             </div>
             <div className='rightSide'><img src={Dp} alt="" /></div>
