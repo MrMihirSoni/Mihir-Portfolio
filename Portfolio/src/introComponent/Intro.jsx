e1de620439ba136377a72ac7b8f10dd70db455ac
Mihir Soni <ms6711534@gmail.com>
2024-01-30 15:14:45 +0530

    Bug Fixxed

--- Portfolio/src/introComponent/Intro.jsx
+++ Portfolio/src/introComponent/Intro.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import './intro.css';
-import HandWaveEmoji from '../../assets/HandWaveEmoji.png';
-import Dp from '../../assets/DP-For-Portfolio.png';
+import React from "react";
+import "./intro.css";
+import HandWaveEmoji from "../../assets/HandWaveEmoji.png";
+import Dp from "../../assets/DP-For-Portfolio.png";
+import MihirSoniResume from "./Mihir-Soni-Resume.pdf";
 const Intro = () => {
-    const handleResume = ()=>{
-        window.open('https://drive.google.com/file/d/1vGXdgIsSqLDg4IRzNdubyQ0f0eiZtsEU/view?usp=sharing','_blank')
-        
-    }
-    return (
-        <div className='home' id='home'>
-            <div className='leftSide'>
-                <p><img src={HandWaveEmoji} alt="" /> Hi!</p>
-                <p>I'm <span className='myName'>Mihir Soni</span></p>
-                <p>FrontEnd Developer</p>
-                <div className='homeBtnDiv'>
-                <a href='Mihir-Soni-Resume.pdf' download='Mihir-Soni-Resume.pdf'><button className='homeBtn' onClick={handleResume}>Resume</button></a>
-                </div>
-            </div>
-            <div className='rightSide'><img src={Dp} alt="" /></div>
+  const handleResume = () => {
+    window.open(
+      "https://drive.google.com/file/d/1YoDLwsxuWh3jOHEgTC76LVezh9Y6WI1j/view?usp=sharing",
+      "_blank"
+    );
+  };
+  return (
+    <div className="home" id="home">
+      <div className="leftSide">
+        <p>
+          <img src={HandWaveEmoji} alt="" /> Hi!
+        </p>
+        <p>
+          I'm <span className="myName">Mihir Soni</span>
+        </p>
+        <p>FrontEnd Developer</p>
+        <div className="homeBtnDiv">
+          <a href={MihirSoniResume} download="Mihir-Soni-Resume.pdf">
+            <button className="homeBtn" onClick={handleResume}>
+              Resume
+            </button>
+          </a>
         </div>
-    )
-}
+      </div>
+      <div className="rightSide">
+        <img src={Dp} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
